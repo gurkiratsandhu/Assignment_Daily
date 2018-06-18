8461df0f0852017d0ff5222d4ae6d4c9fe6628c2
gurkiratsandhu <garrys9191@gmail.com>
2018-06-18 14:10:39 +0530

    data type 2

--- assignment4.py
+++ assignment4.py
@@ -1,44 +1,119 @@
-#assignment no.4
-#ques no. 1
-tuple=(1,2,"hello")
-a=len(tuple)
-print(a)
-# ques  no. 2
-a=max(tuple)
-print(a)
-# ques no. 3
-# Python program to multiply all values in the
-# list using traversal
- 
-def multiplyList(myList) :
-     
-    # Multiply elements one by one
-    result = 1
-    for x in myList:
-         result = result * x 
-    return result 
-     
-# Driver code
-list1 = [1, 2, 3] 
-list2 = [3, 2, 4]
-print(multiplyList(list1))
-print(multiplyList(list2))
-# **************************************************************************
-#***************************************************************************
-#sets***********************************************************************
-#ques no. 1
-aa=set()
-num1=input("enter 1st value")
-num2=input("enter 2nd value")
-num3=input("enter 3rd value")
-aa.update([num1,num2,num3])
-print(aa)
-#ques 2
-ds={1,23,4}
-sd={23,4}
-e=ds.difference(sd)
-print(e)
-#ques 3
-q=ds.intersection(sd)
-print(q)
-#ques 4
+#assignment4
+#Q.1- Write a program to create a tuple with different data types and do the following operations. 
+#1. Find the length of tuples
+t=(2,4,3,'a',2.5)
+print(t)
+print(len(t))
+
+
+#Q.2-Find largest and smallest elements of a tuples. 
+t=(8,9,2,7,1)
+print(t)
+print(max(t))
+print(t)
+print(min(t)
+print(t)
+
+#Q.3- Write a program to find the product of all elements of a tuple.
+t=(1,2,3)
+p=1
+p=p*t[0]
+p=p*t[1]
+p=p*t[2]
+print(p)
+
+#sets
+#Q.1- Create two set using user defined values. 
+#1. Calculate difference between two sets.
+s1={1,2,3}
+s2={6,2,4}
+print(s1-s2)
+
+#2. Compare two sets.
+s1{1,2,3}
+s2{1,4,5}
+print(s1^s2)
+
+#3. Print the result of intersection of two sets.
+s1{1,2,3}
+s2{1,4,5}
+print(s1&s2)
+
+#dictionary
+#Q.1- Create a dictionary to store name and marks of 10 students by user input. 
+name=input("enter name: ")
+a=int(input("enter marks: "))
+
+name=input("enter name: ")
+b=int(input("enter marks: "))
+
+name=input("enter name: ")
+c=int(input("enter marks: "))
+
+name=input("enter name: ")
+d=int(input("enter marks: "))
+
+name=input("enter name: ")
+e=int(input("enter marks: "))
+
+name=input("enter name: ")
+f=int(input("enter marks: "))
+
+name=input("enter name: ")
+g=int(input("enter marks: "))
+
+name=input("enter name: ")
+h=int(input("enter marks: "))
+
+name=input("enter name: ")
+i=int(input("enter marks: "))
+
+name=input("enter name: ")
+j=int(input("enter marks: "))
+
+d=[a,b,c,d,e,f,g,h,i,j]
+print(d)
+
+#Q.2-Sort the dictionary created in previous question according to marks.
+name=input("enter name: ")
+a=int(input("enter marks: "))
+
+name=input("enter name: ")
+b=int(input("enter marks: "))
+
+name=input("enter name: ")
+c=int(input("enter marks: "))
+
+name=input("enter name: ")
+d=int(input("enter marks: "))
+
+name=input("enter name: ")
+e=int(input("enter marks: "))
+
+name=input("enter name: ")
+f=int(input("enter marks: "))
+
+name=input("enter name: ")
+g=int(input("enter marks: "))
+
+name=input("enter name: ")
+h=int(input("enter marks: "))
+
+name=input("enter name: ")
+i=int(input("enter marks: "))
+
+name=input("enter name: ")
+j=int(input("enter marks: "))
+
+d=[a,b,c,d,e,f,g,h,i,j]
+print(d.sort())
+print(d)
+
+#Q.3- Count the number of occurrence of each letter in word "MISSISSIPPI". Store count of every letter with the letter in a dictionary.
+l=list("MISSISSIPPI")
+d={}
+d['M']=l.count('M')
+d['I']=l.count('I')
+d['S']=l.count('S')
+d['P']=l.count('P')
+print(d)
